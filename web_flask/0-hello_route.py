f835345c7e60966277b15a8e459a9d79991b943b
DaisyGeraldine <chipanal@hotmail.com>
2022-11-30 22:41:43 -0500

    fix: script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-""" Script that starts a Flask web application """
+""" script that starts a Flask web application """
 from flask import Flask
-
-# new object.
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-# decorator
-@app.route("/", strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """ This method display message"""
-    return "Hello HBNB!"
+    """ display a message """
+    return 'Hello HBNB!'
 
 
-if __name__ == '__main___':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
