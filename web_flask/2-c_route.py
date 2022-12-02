61d452e956aba2cbd18a1de37b27b8b87a950360
DaisyGeraldine <chipanal@hotmail.com>
2022-12-01 21:45:45 -0500

    script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,3 +9,20 @@ app.url_map.strict_slashes = False
 def hello_hbnb():
     """ display a message """
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ display a message HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_isfun(text=None):
+    """ display a message with a text (parameter) """
+    new_text = text.replace('_', ' ')
+    return f'C {new_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
