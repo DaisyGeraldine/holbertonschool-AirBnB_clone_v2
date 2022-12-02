580850206260818e5bf93dac6c9a3daf7d932b86
DaisyGeraldine <chipanal@hotmail.com>
2022-12-01 20:52:02 -0500

    fix: script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starting a Flask web application """
+"""Starting a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
