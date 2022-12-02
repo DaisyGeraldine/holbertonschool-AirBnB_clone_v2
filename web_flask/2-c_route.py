fea50f9ef4c04f08c5cbffbf41f9ad7957c83302
DaisyGeraldine <chipanal@hotmail.com>
2022-12-01 20:40:47 -0500

    fix: modify comment module

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application """
-
+""" Starting a Flask web application """
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
