2d6b23868e4a4c7dda065beca959be60a697fe86
DaisyGeraldine <chipanal@hotmail.com>
2022-12-01 21:46:50 -0500

    script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -17,12 +17,5 @@ def hbnb():
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_isfun(text=None):
-    """ display a message with a text (parameter) """
-    new_text = text.replace('_', ' ')
-    return f'C {new_text}'
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
