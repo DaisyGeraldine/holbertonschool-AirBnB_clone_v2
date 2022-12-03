328a99bd02b03d949ffe76a4d26f74b0bdf560d8
DaisyGeraldine <chipanal@hotmail.com>
2022-12-03 18:57:32 -0500

    fix: correct code to error-proof

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -19,7 +19,7 @@ def remove_close(self):
 def states_list():
     """  list of all State objects present in DBStorage """
     dict_states = storage.all(State)
-    return render_template('9-states.html', states=dict_states)
+    return render_template('9-states.html', states=dict_states, index="")
 
 
 @app.route('/states/<id>')
@@ -27,6 +27,7 @@ def states_id_list(id):
     """  list of City objects linked to the State sorted by name """
     dict_states = storage.all(State)
     find_name = dict_states.get('State.{}'.format(id), None)
+    print(find_name)
     dict_city = storage.all(City)
     return render_template('9-states.html', index=id,
                            cities=dict_city, states=find_name)
