0249fc87705f89abb11c3df5ae25c6775be14fbf
DaisyGeraldine <chipanal@hotmail.com>
2022-12-03 04:59:01 -0500

    fix: modify pycodestyle

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,19 +15,13 @@ def remove_close(self):
     storage.close()
 
 
-@app.route('/states_list')
-def states_list():
-    """ list of all State objects present in DBStorage sorted by name """
-    dict_states = storage.all(State)
-    return render_template('7-states_list.html', states=dict_states)
-
-
 @app.route('/cities_by_states')
 def cities_list():
     """ load all cities of a State """
     dict_states = storage.all(State)
     dict_city = storage.all(City)
-    return render_template('8-cities_by_states.html', cities=dict_city, states=dict_states)
+    return render_template('8-cities_by_states.html',
+                           cities=dict_city, states=dict_states)
 
 
 if __name__ == '__main__':
