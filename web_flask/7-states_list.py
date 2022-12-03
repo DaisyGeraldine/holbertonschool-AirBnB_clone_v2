b6548400ba926a55030463569d5cff2173918f39
DaisyGeraldine <chipanal@hotmail.com>
2022-12-02 22:39:52 -0500

    show List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application """
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def remove_close():
+    """ tear down database """
+    storage.close()
+
+
+@app.route('/states_list')
+def states_list():
+    """ display a HTML page: list of all State objects present in DBStorage """
+    dict_states = storage.all(State)
+    return render_template('7-states_list.py', states=dict_states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
